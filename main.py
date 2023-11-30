2a700538835cd3ff517e4f38f06c5c07e5520995
Пользователь <shohjaxondehqonboyev59@gmail.com>
2023-11-30 15:19:15 +0500

    first commit

---
+++ main.py
@@ -0,0 +1,32 @@
+
+
+
+a = int(input("togri soni kiriting :"))
+
+if a//2:
+    print("raxmat")
+
+
+
+else:
+   # print("notogri")
+
+
+b = input("26")
+print(f"raxmat  {acev }\nruxsat")
+
+
+
+age = int(input("yoshingizni kiriting: "))
+
+if (age < 4) + (age > 60):
+    print("bepul")
+
+elif age < 18 :
+     print("1o ming so'm")
+
+elif age > 18 :
+    print("20 ming sum")
+
+else:
+       print("....")
